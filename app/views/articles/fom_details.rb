1c1e348d6902c7247060b16cc99ea0347bf3ac72
Shirshir <shishir.nandibatla@gmail.com>
2016-01-27 20:17:05 -0700

    first commit

---
+++ app/views/articles/fom_details.rb
@@ -0,0 +1,44 @@
+before fliter - 
+befoer_action :method_name 
+
+
+def method_name
+p "asdfasd"
+end
+
+after filiter
+aroutnd filter
+
+
+
+form_for
+
+def new
+	@article = Article.new
+end
+
+
+//@article = {}
+
+
+
+# /articles/1/edit
+def edit
+ @artciel = Article.find(params[:id])
+end
+
+#Article(model) -- articles(table name)
+@articles = {:id => 1, :title => "title", :description => 'asdfasdfas'}
+
+
+
+
+
+
+
+
+
+form_tag
+
+
+
